faaa763c97da4bd27446344c9c4067f8baee6a3c
Greg Barnes <gregdbarnes@gmail.com>
2018-02-09 11:19:40 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import Connect from './containers/Connect';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Connect />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: 'React and Redux are integrated (from root reducer)'
+};
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;
